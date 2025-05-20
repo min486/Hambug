09bcf03925a50b28c90ff0ea019232c288082398
min486 <smh92333@gmail.com>
2025-05-20 16:10:47 +0900

    design: 디자인 공통화 작업 (TextStyle, Color, Padding)

---
+++ app/src/main/java/desktop/hambug/ui/theme/Padding.kt
@@ -0,0 +1,16 @@
+package desktop.hambug.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Padding {
+    val none = 0.dp
+    val xs = 2.dp
+    val s = 4.dp
+    val m = 6.dp
+    val l = 8.dp
+    val xl = 10.dp
+    val xxl = 16.dp
+    val xxxl = 24.dp
+    val xxxxl = 32.dp
+    val xxxxxl = 48.dp
+}
